d1a0a1c146710d5ae3b4174984295ce380c6f1f5
Nathan Tryon <nstryon@ncsu.edu>
2020-02-14 19:11:25 -0500

    updating strings

--- GameStrings.java
+++ GameStrings.java
@@ -1,11 +1,18 @@
 class GameStrings {
 	
+	public static final String GAME_BEGIN = "ZORK I: The Great Underground Empire"
+		+ "\nCopyright (c) 1981, 1982, 1983 Infocom, Inc. All rights reserved."
+		+ "\n ZORK is a registered trademark of Infocom, Inc."
+		+ "\n Revision 88 / Serial number 840726";	
 	
 	public static final String DESC_WEST_OF_HOUSE = "You are standing in an open field west of a white house, with a boarded front door."
 		+ "\nThere is a small mailbox here.";
-	public static final String DESC_NORTH_OF_HOUSE = "";
-	public static final String DESC_BEHIND_HOUSE = "";
-	public static final String DESC_SOUTH_OF_HOUSE = "";
+	public static final String DESC_NORTH_OF_HOUSE = "You are facing the north side of a white house. There is no door here, "
+		+ "and all the windows are boarded up. To the north a narrow path winds through the trees.";
+	public static final String DESC_BEHIND_HOUSE = "You are behind the white house. A path leads into the forest to the east. "
+		+ "In one corner of the house there is a small window which is slightly ajar.";
+	public static final String DESC_SOUTH_OF_HOUSE = "You are facing the south side of a white house. There is no door here, "
+		+ "and all the windows are boarded.";
 	public static final String DESC_KITCHEN = "";
 	public static final String DESC_ATTIC = "";
 	public static final String DESC_LIVING_ROOM = "";
